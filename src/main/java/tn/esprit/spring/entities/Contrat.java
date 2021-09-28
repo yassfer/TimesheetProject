bcf0000b33ae737d10421417277dc215c70730c3
nadakd <nada.korkad@esprit.tn>
2021-09-28 19:35:21 +0100

    test

--- src/main/java/tn/esprit/spring/entities/Contrat.java
+++ src/main/java/tn/esprit/spring/entities/Contrat.java
@@ -13,7 +13,8 @@ import javax.persistence.TemporalType;
 
 @Entity
 public class Contrat implements Serializable {
-	//
+
+	//test
 	private static final long serialVersionUID = 6191889143079517027L;
 
 	@Id
