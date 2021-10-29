52038e6f3b422e0e775ad8e7d6c7dfce45a24ac6
nadakd <nada.korkad@esprit.tn>
2021-10-29 21:41:41 +0100

    test unitaire

--- src/test/java/tn/esprit/spring/TimesheetServiceImplTest.java
+++ src/test/java/tn/esprit/spring/TimesheetServiceImplTest.java
@@ -1,9 +1,8 @@
 package tn.esprit.spring;
 import static org.junit.Assert.*;
 
-
 import java.util.List;
-
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,9 +12,11 @@ import tn.esprit.spring.services.TimesheetServiceImpl;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
 
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TimesheetServiceImplTest {
+
 	@Autowired
 	  private  TimesheetServiceImpl timesheetServiceImpl;
 	
@@ -25,9 +26,9 @@ public class TimesheetServiceImplTest {
 	public void TestajouterMission() {
 		Mission mission = new Mission("test","test");
 		int missionadd = timesheetServiceImpl.ajouterMission(mission);
+		timesheetServiceImpl.deleteMissionById(mission.getId());
 		assertTrue(mission.getName().equals("test"));
 		assertEquals(missionadd,mission.getId());
-		timesheetServiceImpl.deleteMissionById(mission.getId());
 		
 		}
 	
@@ -42,45 +43,35 @@ public class TimesheetServiceImplTest {
 		
 		}
 	
-	/*
+	
 	@Test
-  public void TestfindAllMissionBydepartementJPQL()
-  {   
-      //test
+    public void TestgetAllMissions()
+    {   
+        //test
+        List<Mission> misList = timesheetServiceImpl.getAllMissions();
+        assertEquals(5, misList.size());
+        
+    }
+	
+	@Test
+    public void TestfindAllMissionByEmployeJPQL()
+    {   
+        //test
+		Mission misList = (Mission)timesheetServiceImpl.findAllMissionByEmployeJPQL(1);
+        assertNotEquals("Lokesh", misList.getName());
+        assertEquals("test", misList.getDescription());
+       
+    } 
+
+	@Test
+    public void TestfindAllMissionBydepartementJPQL()
+    {   
+        //test
 		Mission misList = (Mission)timesheetServiceImpl.findAllMissionBydepartementJPQL(1);
 		assertEquals("test", misList.getName());
-    
-     
-  } */
-	
-	@Test
-  public void TestgetAllMissions()
-  {   System.out.println("Running testList...");
-      //test
-      List<Mission> misList = timesheetServiceImpl.getAllMissions();
-      assertEquals(5, misList.size());
       
-  }
-	/*
-	@Test
-  public void TestfindAllMissionByEmployeJPQL()
-  {   
-      //test
-		Mission misList = (Mission)timesheetServiceImpl.findAllMissionByEmployeJPQL(1);
-      assertNotEquals("Lokesh", misList.getName());
-      assertEquals("test", misList.getDescription());
-     
-  } */
-	
-	@Test
-  public void TestgetMissionById()
-  {   
-      //test
-		Mission misList = (Mission)timesheetServiceImpl.getMissionById(9);
-      assertEquals("test", misList.getName());
-      assertEquals("test", misList.getDescription());
-  }
-   /*
+    } 
+   
 	
 	@Test
 	public void testdeleteMissionById() {
@@ -88,9 +79,18 @@ public class TimesheetServiceImplTest {
 	  //  timesheetServiceImpl.deleteMissionById(6);
      Mission mission = timesheetServiceImpl.getMissionById(10);
       assertThat(mission).isNull();
- 
-	} 
-	*/
+	}
+	
+	
+	@Test
+    public void TestgetMissionById()
+    {   
+        //test
+		Mission misList = (Mission)timesheetServiceImpl.getMissionById(9);
+        assertEquals("test", misList.getName());
+        assertEquals("test", misList.getDescription());
+    }
+     
 	    
 	 @Test
 	    public void testmettreAjourDescriptionByMissionId() {
@@ -104,3 +104,4 @@ public class TimesheetServiceImplTest {
 	 
 	 
 	}
+	
